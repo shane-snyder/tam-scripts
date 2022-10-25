3b1edf4ba2051d00b3b4f1517962fd81f8f3e005
shane-snyder <shanesnyder2424@gmail.com>
2022-10-25 17:01:16 -0400

    Added color coding

--- etcd/etcd_errors/etcd_errors.sh
+++ etcd/etcd_errors/etcd_errors.sh
@@ -35,6 +35,8 @@ do
     if [ $(omc logs $POD -c etcd -n openshift-etcd| grep 'took too long' | wc -l) -gt 0 ]
     then
         echo -e "\e[1;31mETCD WARNING APPLY ENTRIES TOOK TOO LONG\e[1;m \nAs per required by its consensus protocol implementation, after a majority of etcd members agree to commit a request, each etcd server applies the request to its data store and persists the result to disk. If the average request duration exceeds 100 milliseconds, etcd will warn entries request took too long"
+        echo -e "RESOURCES
+        https://etcd.io/docs/v3.5/faq/#what-does-the-etcd-warning-apply-entries-took-too-long-mean \n"
         omc get pods -n openshift-etcd|grep etcd|grep -v quorum|while read POD line; do echo $POD && omc logs $POD -c etcd -n openshift-etcd| grep 'took too long'| cut -c -16 | uniq -c; done
         echo -e "\n"
         break
@@ -49,6 +51,9 @@ do
     if [ $(omc logs $POD -c etcd -n openshift-etcd| grep 'clock difference'| wc -l) -gt 0 ]
     then
         echo -e "\e[1;31mETCD CLOCK DIFFERENCE\e[1;m\nAs per required by its consensus protocol implementation, after a majority of etcd members agree to commit a request, each etcd server applies the request to its data store and persists the result to disk. If the average request duration exceeds 100 milliseconds, etcd will warn entries request took too long"
+        echo -e "RESOURCES
+        https://access.redhat.com/solutions/6371621
+        https://docs.openshift.com/container-platform/4.6/installing/install_config/installing-customizing.html?extIdCarryOver=true&sc_cid=701f2000001Css5AAC#installation-special-config-chrony_installing-customizing \n"
         omc get pods -n openshift-etcd|grep etcd|grep -v quorum|while read POD line; do echo $POD && omc logs $POD -c etcd -n openshift-etcd| grep 'clock difference'| cut -c -16 | uniq -c; done
         echo -e "\n"
     else 
@@ -64,6 +69,8 @@ do
         echo -e "\e[1;31mETCD DATABASE SPACE EXCEEDED\e[1;m \nWithout periodically compacting this history (e.g., by setting --auto-compaction), etcd will eventually exhaust its storage space. If etcd runs low on storage space, it raises a space quota alarm to protect the cluster from further writes. So long as the alarm is raised, etcd responds to write requests with the error mvcc: database space exceeded.
         In RHOCP 4.x, history compaction is performed automatically every five minutes and leaves gaps in the back-end database. This fragmented space is available for use by etcd, but is not available to the host file system. You must defragment etcd to make this space available to the host file system.
         Starting in RHOCP 4.9.z, defragmentation occurs automatically."
+        echo -e "RESOURCES
+        https://etcd.io/docs/v3.5/faq/#what-does-mvcc-database-space-exceeded-mean-and-how-do-i-fix-it \n"
         omc get pods -n openshift-etcd|grep etcd|grep -v quorum|while read POD line; do echo $POD && omc logs $POD -c etcd -n openshift-etcd| grep 'database space exceeded'| cut -c -16 | uniq -c; done
         echo -e "\n"
     else 
@@ -78,6 +85,8 @@ do
     then
         echo -e "\e[1;31mETCD LEADERSHIP CHANGES AND FAILURES\e[1;m\nLeadership changes are expected only during installations, upgrades or machine config operations. During day to day operation, it must be avoided.
         During the leader election the cluster cannot process any writes. Write requests sent during the election are queued for processing until a new leader is elected. Until a new leader is elected, we are going to observe instabilities, slow response times and unexpected behaviors affecting RHOCP control plane."
+        echo -e "RESOURCES
+        https://etcd.io/docs/v3.5/op-guide/failures/#leader-failure \n"
         omc get pods -n openshift-etcd|grep etcd|grep -v quorum|while read POD line; do echo $POD && omc logs $POD -c etcd -n openshift-etcd| grep 'leader changed'| cut -c -16 | uniq -c; done
         echo -e "\n"
     else 
